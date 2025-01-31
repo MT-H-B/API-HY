8bb3907356ac64222df90c999e7b2cc318e975d7
DESKTOP-TTQJSPO\suglo <sumglory@naver.com>
2025-02-01 01:20:07 +0900

    스프링부트 6-1장 예제

---
+++ jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -0,0 +1,34 @@
+package com.springboot.jpa.data.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime creadedAt;
+
+    private LocalDateTime updatedAt;
+
+    public void setUpdateAt(LocalDateTime now) {
+    }
+}
